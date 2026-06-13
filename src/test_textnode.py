29c8b623f6e95948237af7eea173fdf56adff54e
Gaboonv1 <empathysgrave@gmail.com>
2026-06-13 16:20:51 -0400

    added htmlnode for diffenent inline text, props to html can return a formatted string. Created test for htmlnode to see if it returns the correct strings.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -6,6 +6,22 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+   
+    def test_not_eq(self):
+        node =  TextNode("This is a text node", TextType.BOLD)  
+        node2 = TextNode("This is a text node", TextType.ITALIC) 
+        self.assertNotEqual(node, node2)
+
+    def test_diff_text(self):
+        node =  TextNode("This is a text node", TextType.BOLD)  
+        node2 = TextNode("Hello world", TextType.ITALIC) 
+        self.assertNotEqual(node, node2)
+
+    def test_url(self):
+        node =  TextNode("This is a text node", TextType.BOLD, url="https://example.com")  
+        node2 = TextNode("Hello world", TextType.BOLD, url = None) 
+        self.assertNotEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
